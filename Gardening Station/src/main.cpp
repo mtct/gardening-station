0002784a3ee5d6c23afc41d84dbabf62eaa9536b
mtct <51030179+mtct@users.noreply.github.com>
2023-03-28 21:57:03 +0200

    Read data from DHT sensor

--- Gardening Station/src/main.cpp
+++ Gardening Station/src/main.cpp
@@ -1,12 +1,39 @@
 #include <Arduino.h>
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
+
+#define DHT_SENSOR_TYPE DHT22
+#define DHT_SENSOR_PIN  25
+
+DHT dht_sensor(DHT_SENSOR_PIN, DHT_SENSOR_TYPE);
 
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(9600);
+  dht_sensor.begin();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-    Serial.println("Hello world!");
-    delay(1000);
+  // Read humidity from sensor
+  float humidity  = dht_sensor.readHumidity();
+  // Read temperature from sensor
+  float temperature = dht_sensor.readTemperature();
+
+  // check successful read of sensor values
+  if (isnan(humidity) || isnan(temperature)) {
+    Serial.println("Failed to read from DHT sensor!");
+  } 
+  else {
+    Serial.print("Humidity: ");
+    Serial.print(humidity);
+    Serial.print("%");
+
+    Serial.print("  |  ");
+
+    Serial.print("Temperature: ");
+    Serial.print(temperature);
+    Serial.print("°C \n");
+  }
+
+  // wait a 5 seconds between readings
+  delay(5000);
 }
