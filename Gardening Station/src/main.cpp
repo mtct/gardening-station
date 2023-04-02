c978fa7b75c8c00419c7a937298a4a882a6992fb
mtct <51030179+mtct@users.noreply.github.com>
2023-04-02 17:53:45 +0200

    Read resistence data from soil sensors

--- Gardening Station/src/main.cpp
+++ Gardening Station/src/main.cpp
@@ -4,6 +4,9 @@
 
 #define DHT_SENSOR_TYPE DHT22
 #define DHT_SENSOR_PIN  25
+#define SOIL_SENSOR_1_PIN  33
+#define SOIL_SENSOR_2_PIN  32
+#define SOIL_SENSOR_3_PIN  35
 
 DHT dht_sensor(DHT_SENSOR_PIN, DHT_SENSOR_TYPE);
 
@@ -18,6 +21,13 @@ void loop() {
   // Read temperature from sensor
   float temperature = dht_sensor.readTemperature();
 
+  // Read soil resistance from first sensor
+  int sensor1_soil_humidity = analogRead(SOIL_SENSOR_1_PIN);
+  // Read soil resistance from second sensor
+  int sensor2_soil_humidity = analogRead(SOIL_SENSOR_2_PIN);
+  // Read soil resistance from third sensor
+  int sensor3_soil_humidity = analogRead(SOIL_SENSOR_3_PIN);
+
   // check successful read of sensor values
   if (isnan(humidity) || isnan(temperature)) {
     Serial.println("Failed to read from DHT sensor!");
@@ -31,7 +41,29 @@ void loop() {
 
     Serial.print("Temperature: ");
     Serial.print(temperature);
-    Serial.print("°C \n");
+    Serial.print("°C");
+
+    Serial.print("\n");
+  }
+
+  if (isnan(sensor1_soil_humidity) || isnan(sensor2_soil_humidity) || isnan(sensor3_soil_humidity)) {
+    Serial.println("Failed to read from soil sensors!");
+  } 
+  else{
+    Serial.print("Soil humidity 1: ");
+    Serial.print(sensor1_soil_humidity);
+
+    Serial.print("  |  ");
+
+    Serial.print("Soil humidity 2: ");
+    Serial.print(sensor2_soil_humidity);
+
+    Serial.print("  |  ");
+
+    Serial.print("Soil humidity 3: ");
+    Serial.print(sensor3_soil_humidity);
+
+    Serial.print("\n");
   }
 
   // wait a 5 seconds between readings
